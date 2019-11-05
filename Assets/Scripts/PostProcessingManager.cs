50252da00c3ac890ace03880f6af91850ff9d428
Aleskandor <37109130+Aleskandor@users.noreply.github.com>
2019-11-05 12:35:47 +0100

    Shaders and Effects
    Added some post processing effects, some new models and particle effects.

---
+++ Assets/Scripts/PostProcessingManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingManager : MonoBehaviour
+{
+    PostProcessVolume pV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pV = GetComponent<PostProcessVolume>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pV.enabled && Input.GetKeyDown(KeyCode.P))
+        {
+            pV.enabled = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            pV.enabled = true;
+        }
+    }
+}
