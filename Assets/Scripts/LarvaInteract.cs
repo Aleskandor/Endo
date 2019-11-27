e1d00a6a09a700f5cb7c80208c6a78791eafd7a1
CosmicEgg <johnholmberg_@hotmail.com>
2019-11-27 09:30:53 +0100

    river fixes

--- Assets/Scripts/LarvaInteract.cs
+++ Assets/Scripts/LarvaInteract.cs
@@ -9,88 +9,90 @@ public class LarvaInteract : MonoBehaviour
     public Transform larva;
     public Transform pivot;
     public Transform endPivot;
+    public Transform startPivot;
 
     private Vector3 mOffset;
-    private Vector3 stickStart;
-    private Vector3 stickEnd;
+    private Vector3 stickStartPos;
+    private Vector3 stickMoveVector;
     private bool stickAtLarva;
     private float mYCoord, mZCoord;
-
+    private float mouseStickSensitiveity;
 
     private void Start()
     {
         HasCrossed = false;
-
-        stickStart = new Vector3(72.4f, 3.99f, -28.61f);
-        stickEnd = new Vector3(71.3f, 3.99f, -28.61f);
+        mouseStickSensitiveity = 0.1f;
+        stickMoveVector = (endPivot.position - startPivot.position).normalized;
+        stickStartPos = startPivot.position + stickMoveVector *0.8f;
+        pivot.position = stickStartPos;
     }
 
     private void Update()
     {
-        Constraints();
-        LarvaMovement();
+        StickMove();
+        //LarvaMovement();
     }
 
-    private void OnMouseDown()
+    //private void OnMouseDown()
+    //{
+    //    mYCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).y;
+    //    mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+    //    mOffset = gameObject.transform.position - GetMouseWorldPos();
+    //}
+
+    //private void OnMouseDrag()
+    //{
+    //    transform.position = GetMouseWorldPos() + mOffset;
+    //}
+
+    //private Vector3 GetMouseWorldPos()
+    //{
+    //    Vector3 mousePoint = Input.mousePosition;
+
+    //    mousePoint.y = mYCoord;
+    //    mousePoint.z = mZCoord;
+
+    //    return Camera.main.ScreenToWorldPoint(mousePoint);
+    //}
+
+
+
+    //private void LarvaMovement()
+    //{
+    //    if (!HasCrossed)
+    //    {
+    //        if (transform.position.x >= stickStart.x)
+    //            larva.position = Vector3.MoveTowards(larva.position, new Vector3(pivot.position.x, pivot.position.y, pivot.position.z), .5f * Time.deltaTime);
+    //        if (pivot.position == larva.position)
+    //            stickAtLarva = true;
+    //        if (stickAtLarva && !(transform.position.x <= stickEnd.x))
+    //            larva.position = pivot.position;
+
+    //        if (transform.position.x <= stickEnd.x && stickAtLarva)
+    //            larva.position = Vector3.MoveTowards(larva.position, new Vector3(endPivot.position.x, endPivot.position.y, endPivot.position.z), .5f * Time.deltaTime);
+    //        if (larva.position == endPivot.position)
+    //        {
+    //            stickAtLarva = false;
+    //            HasCrossed = true;
+    //        }
+    //    }
+    //}
+    private void StickMove()
     {
-        mYCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).y;
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
 
-        mOffset = gameObject.transform.position - GetMouseWorldPos();
-    }
-
-    private void OnMouseDrag()
-    {
-        transform.position = GetMouseWorldPos() + mOffset;
-    }
-
-    private Vector3 GetMouseWorldPos()
-    {
-        Vector3 mousePoint = Input.mousePosition;
-
-        mousePoint.y = mYCoord;
-        mousePoint.z = mZCoord;
-
-        return Camera.main.ScreenToWorldPoint(mousePoint);
-    }
-
-    private void Constraints()
-    {
-        //Constraints for the Larva tree
-        if (transform.position.x > stickStart.x)
-            transform.position = new Vector3(stickStart.x, transform.position.y, transform.position.z);
-        if (transform.position.y > stickStart.y)
-            transform.position = new Vector3(transform.position.x, stickStart.y, transform.position.z);
-        if (transform.position.z < stickStart.z)
-            transform.position = new Vector3(transform.position.x, transform.position.y, stickStart.z);
-
-        //Constraints for the other tree
-        if (transform.position.x < stickEnd.x)
-            transform.position = new Vector3(stickEnd.x, transform.position.y, transform.position.z);
-        if (transform.position.y < stickEnd.y)
-            transform.position = new Vector3(transform.position.x, stickEnd.y, transform.position.z);
-        if (transform.position.z > stickEnd.z)
-            transform.position = new Vector3(transform.position.x, transform.position.y, stickEnd.z);
-    }
-
-    private void LarvaMovement()
-    {
-        if (!HasCrossed)
+        if(Input.GetAxis("Mouse X") <=0)
         {
-            if (transform.position.x >= stickStart.x)
-                larva.position = Vector3.MoveTowards(larva.position, new Vector3(pivot.position.x, pivot.position.y, pivot.position.z), .5f * Time.deltaTime);
-            if (pivot.position == larva.position)
-                stickAtLarva = true;
-            if (stickAtLarva && !(transform.position.x <= stickEnd.x))
-                larva.position = pivot.position;
-
-            if (transform.position.x <= stickEnd.x && stickAtLarva)
-                larva.position = Vector3.MoveTowards(larva.position, new Vector3(endPivot.position.x, endPivot.position.y, endPivot.position.z), .5f * Time.deltaTime);
-            if (larva.position == endPivot.position)
-            {
-                stickAtLarva = false;
-                HasCrossed = true;
-            }
+            pivot.position = Vector3.MoveTowards(pivot.position, endPivot.position, Time.deltaTime);
         }
+        if (Input.GetAxis("Mouse X") >= 0)
+        {
+            pivot.position = Vector3.MoveTowards(pivot.position, startPivot.position, Time.deltaTime);
+        }
+
+        Debug.Log("stick: " +pivot.position);
+        Debug.Log("start: " + startPivot.position);
+        Debug.Log("end: " + endPivot.position);
+
     }
 }
