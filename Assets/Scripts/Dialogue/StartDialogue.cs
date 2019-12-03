978f9abd93289cd55137fefbb18c1800cc80c092
reacotay <anton.hallqvist1@gmail.com>
2019-12-03 10:17:35 +0100

    Dialogue and other fixes

---
+++ Assets/Scripts/Dialogue/StartDialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDialogue : MonoBehaviour
+{
+    private DialogueTrigger DT;
+    void Start()
+    {
+        DT = gameObject.GetComponent<DialogueTrigger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            DT.TriggerDialogue();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Human" || other.name == "Dog")
+            DT.TriggerDialogue();
+    }
+}
