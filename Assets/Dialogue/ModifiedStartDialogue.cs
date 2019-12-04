0f3b32b8283ce648decada556db4aa2e012ff679
CosmicEgg <johnholmberg_@hotmail.com>
2019-12-04 15:45:24 +0100

    CampScene fix

---
+++ Assets/Dialogue/ModifiedStartDialogue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifiedStartDialogue : MonoBehaviour
+{
+    private DialogueTrigger DT;
+    private bool speechOver = false;
+    public bool replayable = false;
+    void Start()
+    {
+        DT = gameObject.GetComponent<DialogueTrigger>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.name == "Human" && !speechOver) || (other.name == "Dog" && !speechOver))
+        {
+            speechOver = true;
+            DT.TriggerDialogue();
+        }
+    }
+}
