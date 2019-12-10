4b9c1d87c510017f1f2ba5da97d802f5d8e71107
CosmicEgg <johnholmberg_@hotmail.com>
2019-12-10 13:57:48 +0100

    RiverScene (update)
    - Stitched together the ground
    - Fixed floating trees

---
+++ Assets/Dialogue/CaterpillarStartDialogue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaterpillarStartDialogue : MonoBehaviour
+{
+    private DialogueTrigger DT;
+    private bool speechOver = false;
+    public bool replayable = false;
+
+    void Start()
+    {
+        DT = gameObject.GetComponent<DialogueTrigger>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Human" && !speechOver && FindPrintsDialog.over)
+        {
+            speechOver = true;
+            DT.TriggerDialogue();
+        }
+    }
+}
