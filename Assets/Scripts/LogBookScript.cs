4971423462aea3111414076d714b1aa12d1c5354
reacotay <anton.hallqvist1@gmail.com>
2019-12-11 15:20:37 +0100

    Added Rough Logbook

---
+++ Assets/Scripts/LogBookScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogBookScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text text;
+    public Animator animator;
+
+    private bool open;
+
+    void Start()
+    {
+        open = false;
+        AddToLog("- Do More Stuff");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && !open)
+        {
+            animator.SetBool("IsOpen", true);
+            open = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && open)
+        {
+            animator.SetBool("IsOpen", false);
+            open = false;
+        }
+
+
+    }
+
+    public void AddToLog(string t)
+    {
+        text.text = text.text + "\n" + t;
+    }
+}
