97a265e36fddb0e3b3dc2777c8f5adbc529d76e9
CosmicEgg <johnholmberg_@hotmail.com>
2019-12-17 15:24:28 +0100

    dialog forset

--- Assets/Dialogue/DialogueManager.cs
+++ Assets/Dialogue/DialogueManager.cs
@@ -33,7 +33,7 @@ public class DialogueManager : MonoBehaviour
         {
             if (child.gameObject.name == dialogue.name)
                 child.gameObject.SetActive(true);
-            else if (child.gameObject.name == "Spirit" && dialogue.name == "????")
+            else if (child.gameObject.name == "Forest Spirit" && dialogue.name == "????")
                 child.gameObject.SetActive(true);
             else
                 child.gameObject.SetActive(false);
